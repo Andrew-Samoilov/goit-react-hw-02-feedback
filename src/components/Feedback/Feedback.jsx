f7fd2efaa804d992ec8f31516058e3d67fff7ff4
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-02-20 11:58:06 +0200

    test

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,7 +1,17 @@
-import { Title } from "./Feedback.styled";
+import { Title, Section, Btn } from "./Feedback.styled";
 
 export const Feedback = () => {
-    return (<Title>
-        Feedback
-    </Title>);
+    return (<Section><Title>
+        Feed back
+    </Title>
+        <h2>Please leave feedback</h2>
+        <Btn>Good</Btn>
+        <Btn>Neutral</Btn>
+        <Btn>Bad</Btn>
+       
+        <h3>Statistics</h3>
+        <span>Good</span>
+        <span>Neutral</span>
+        <span>Bad</span>
+    </Section>);
 }
--- src/components/Feedback/Feedback.styled.jsx
+++ src/components/Feedback/Feedback.styled.jsx
@@ -1,10 +1,22 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
-    height: 100vh;
+    height: 5vh;
             display: flex;
             justify-Content: center;
             align-Items: center;
             font-Size: 40;
             color: #010101;
+`;
+
+export const Section = styled.section`
+    padding: 5vh; 
+    max-width: 50vh;
+    background-color: lightgreen; 
+    margin-left: auto;
+    margin-right: auto;
+`;
+
+export const Btn = styled.button`
+    padding: 1vh;
 `;
